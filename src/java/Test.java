612bbedb08f3d7545ee89acf11e414c5ba3fdc1a
eclispevs <591755818@qq.com>
2019-09-19 15:20:13 +0800

    添加

--- src/java/Test.java
+++ src/java/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("dddddddddd");
         System.out.println("ffffff");
+        System.out.println("oooooooooooo");
     }
 }
