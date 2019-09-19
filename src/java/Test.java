08365eb995a3bf6dde890a036d1190eec5ccc8e6
eclispevs <591755818@qq.com>
2019-09-19 15:11:50 +0800

    Initial commit

---
+++ src/java/Test.java
@@ -0,0 +1,11 @@
+package java;
+
+/**
+ * @author wk
+ * @date 2019/9/19  15:08
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("dddddddddd");
+    }
+}
